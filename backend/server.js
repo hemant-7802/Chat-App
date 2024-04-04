73cce27cc204e53d520c309833caaa1ee4b86990
hemant-7802 <hsharma9667@gmail.com>
2024-04-04 22:33:49 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 import express from "express";
-import dotenv from "dotenv"
-dotenv.config();
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("Hello world!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.listen(PORT, (req,res) => {
-    console.log(`Server is running on port number ${PORT}`);
-})
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port number ${PORT}`);
+});
