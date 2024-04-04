cea659a9bb01ea824c796d1e24c2e7fed099fb35
gaayush234 <goualaayush@gmail.com>
2024-04-04 12:19:10 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv"
+dotenv.config();
+
+const PORT = process.env.PORT
+
+const app = express();
+
+app.get("/", (req,res) => {
+    res.send("Hello world!")
+})
+
+app.listen(PORT, (req,res) => {
+    console.log(`Server is running on port number ${PORT}`);
+})
