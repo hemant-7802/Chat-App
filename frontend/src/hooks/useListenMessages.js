58e695890a9bb5642ce2c4cdce18c0828bc6fb35
Hemant Bhargav <143734039+hemant-7802@users.noreply.github.com>
2024-05-02 18:33:29 +0000

    bug solve

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -16,7 +16,7 @@ const useListenMessages = () => {
       setMessages([...messages, newMessage])
     })
 
-    return () => socket.off("newMessage")
+    return () => socket?.off("newMessage")
   }, [socket, setMessages, messages])
 }
 export default useListenMessages
